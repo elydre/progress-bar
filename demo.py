20cdf455fb486f768f7f21a9c89829feac4364bc
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-04-29 20:48:59 +0200

    0.0.4

--- demo.py
+++ demo.py
@@ -1,20 +1,22 @@
 from progress_bar import Bar
+from time import sleep
 
 def progress_loop():
-    for i in range(8000):
+    for i in range(42):
         test.progress(i+1)
+        sleep(0.1)
 
 
 # percentage mode
-test = Bar(8000, "*", "p")
+test = Bar(42, "*", "p")
 progress_loop()
 
 # steps mode
-test = Bar(8000, "-", "s")
+test = Bar(42, "-", "s")
 progress_loop()
 
 # clean mode
-test = Bar(8000, "#", "c")
+test = Bar(42, "#", "c")
 progress_loop()
 
 print()
--- progress_bar.py
+++ progress_bar.py
@@ -16,23 +16,24 @@
 '''
 
 import os
+from ColorPrint import colorprint
 
-version = "0.0.3"
+version = "0.0.4"
 
 class Bar:
-    def __init__(self, nb, char="=", display_mode="p"):
+    def __init__(self, step, char="=", display_mode="p", longueur = 0):
         """
         nb: number of steps
         char: char to display
         display_mode: "p" percents, "s" steps, "c" clean
         """
         
-        self.longueur = os.get_terminal_size().columns - 3
-        self.nb = nb
+        self.longueur = longueur or os.get_terminal_size().columns - 3
+        self.nb = step
         self.char = char
         self.steps = [round(x * (self.longueur / self.nb)) for x in range(self.nb + 1)]
+        self.gradient = [(255, i, 0) for i in range(100, 255)] + [(255 - i, 255, 0) for i in range(255)] + [(0, 255, i) for i in range(255)]
         self.display_mode = display_mode
-
         print("progress bar initialized")
 
     def go_up(self):
@@ -40,8 +41,8 @@ class Bar:
         print(end="\033[K")
 
     def get_display(self, step): # you can change this function to display custom things
-        if self.display_mode == "p": return f" {round(step / self.nb * 100)}% "
-        if self.display_mode == "s": return f" {step}/{self.nb} "
+        if "p" in self.display_mode: return f" {round(step / self.nb * 100)}% "
+        if "s" in self.display_mode: return f" {step}/{self.nb} "
         return ""
 
     def progress(self, step):
@@ -58,7 +59,8 @@ class Bar:
                 to_display = ""
 
             elif i < self.steps[step]:
-                print(self.char, end="")
+                colorprint(self.char, self.gradient[round(len(self.gradient) * (i / self.longueur))], "k")
+
             else:
                 print(" ", end="")
 
